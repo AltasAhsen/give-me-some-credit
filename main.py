95997617a0327a8b75e47cdd3f922d8a726e94e1
Ahsen Altaş <ahsenaltas@Ahsen-MacBook-Air.local>
2025-06-29 18:48:54 +0300

    replaced PCA with composite late payment score. Analyzed outliers impact. Optimized using IQR filtering

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # %% [markdown]
-# # Features of Dataset
+# Features of Dataset
 # 
 # **id** <br/>
 # **SeriousDlqin2yrs** : Whether the debtor has experienced serious financial difficulties in the last two years <br/>
@@ -14,16 +14,24 @@
 # **NumberOfTime60-89DaysPastDueNotWorse** <br/>
 # **NumberOfDependents** <br/>
 
+# %% [markdown]
+# Model Results <br/> <br/>
+# Before outlier correction <br/>
+# Decision Tree: F1 = 0.81, ROC AUC = 0.78 <br/>
+# QDA: F1 = 0.60, ROC AUC = 0.62
+
 # %%
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
+from imblearn.over_sampling import SMOTE
 from sklearn.preprocessing import StandardScaler
-from sklearn.tree import DecisionTreeClassifier,export_text
-from sklearn.metrics import f1_score, roc_auc_score, roc_curve
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier,export_text
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.metrics import f1_score, roc_auc_score, roc_curve, confusion_matrix, ConfusionMatrixDisplay
+from scipy.stats import zscore
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -39,24 +47,7 @@ number_of_dependents_mode = df['NumberOfDependents'].mode()[0]
 df['NumberOfDependents'].fillna(number_of_dependents_mode, inplace=True)
 print(df.info())
 
-#%% Visualizing the data
-for i in df.columns:
-    plt.figure(figsize=(10, 6))
-    sns.histplot(df[i], kde=True, bins=30, color='blue')
-    plt.title(f'{i} Distribution')
-    plt.xlabel(i)
-    plt.ylabel('Frequency')
-    plt.show() # It is an unbalanced dataset. Around 140.000 times 0 in SeriousDlqin2yrs and 20.000 times 1
-
-for i in df.columns:
-    plt.figure(figsize=(10, 6))
-    sns.stripplot(df[i], alpha=0.7, color='blue')
-    plt.title(f'{i} Distribution')
-    plt.xlabel(i)
-    plt.ylabel('Frequency')
-    plt.show()
-
-
+# %% Examining the data
 for col in df.select_dtypes(include=['float', 'int']).columns:
     print(f"{col}:")
     print(df[col].describe())
@@ -72,6 +63,13 @@ filtered_df = df[
 print(filtered_df)
 # NumberOfTimes90DaysLate deosn't repeat on NumberOfTime30-59DaysPastDueNotWorse
 
+# %% Visualizing the dataset
+df.hist(bins=30, figsize=(15, 10))
+plt.tight_layout()
+plt.show()
+
+print(df.describe())
+
 # %% Pointing out high correlation features
 plt.figure(figsize=(10,8))
 sns.heatmap(df.corr(), annot=True, fmt=".2f", cmap='coolwarm')
@@ -86,75 +84,42 @@ high_corr_columns = ['NumberOfTime30-59DaysPastDueNotWorse',
                      'NumberOfTimes90DaysLate', 
                      'NumberOfTime60-89DaysPastDueNotWorse']
 
-# %% Creating PCA for high correlation
-#standardization of only these three component, for PCA
-scaler = StandardScaler()
-scaled_data = scaler.fit_transform(df[high_corr_columns])
+# %% Collecting High Correlation Variables in One Variable
+high_corr_table = df[high_corr_columns].describe()
+print(high_corr_table)
 
-#principal component
-pca = PCA(n_components=1)
-principal_component = pca.fit_transform(scaled_data)
-#adding to table
-df['PC1'] = principal_component
-df.drop(columns=high_corr_columns, inplace=True)
-#with this new variable(PC1), 3 column wont dominate the other features
+# since the variance is almost 0 in these variables, PCA is not a good idea
+# I will create one variable for these three, so three variable won't dominate the prediction
+df['LatePaymentScore'] = (
+    df['NumberOfTime30-59DaysPastDueNotWorse'] +
+    df['NumberOfTimes90DaysLate'] +
+    df['NumberOfTime60-89DaysPastDueNotWorse']
+)
+df.drop(columns=['NumberOfTime30-59DaysPastDueNotWorse', 'NumberOfTimes90DaysLate', 'NumberOfTime60-89DaysPastDueNotWorse'], inplace=True)
 
-print(df.head())
-plt.figure(figsize=(10,8))
-sns.heatmap(df.corr(), annot=True, fmt=".2f", cmap='coolwarm')
-plt.title("Correlation Matrix")
-plt.show()
-
-components = pca.components_
-feature_contributions = pd.DataFrame(components, columns=high_corr_columns)
-feature_contributions_percentage = feature_contributions.abs() / feature_contributions.abs().sum(axis=1).values.reshape(-1, 1) * 100
-
-print("PCA Component Contribution Percentages:")
-print(feature_contributions_percentage)
-# It means PC1 = %33 of NumberOfTime30-59DaysPastDueNotWorse + %33 of NumberOfTimes90DaysLate + %33 of NumberOfTime60-89DaysPastDueNotWorse
-print(pca.explained_variance_ratio_)
-# [0.99186925]
+print(df['LatePaymentScore'].describe())
 
 # %% Defining variables
-x_train=df.iloc[:,1:]
+scaler = StandardScaler()
+x=df.iloc[:,1:]
+y=df["SeriousDlqin2yrs"]
 
-y_train=df["SeriousDlqin2yrs"]
-standard_x = pd.DataFrame(scaler.fit_transform(x_train), columns=x_train.columns)
-X_train_split, X_val_split, y_train_split, y_val_split = train_test_split(
-    standard_x, y_train, test_size=0.2, random_state=42
+# in SeriousDlqin2yrs, there are 0 around 140000 and 1 around 20000. To fix this imbalance
+smote = SMOTE(random_state=42)
+X_resampled, y_resampled = smote.fit_resample(x, y)
+
+standard_x = pd.DataFrame(scaler.fit_transform(X_resampled), columns=X_resampled.columns)
+
+X_train, X_val, y_train, y_val = train_test_split(
+    standard_x, y_resampled, test_size=0.2, random_state=42
 )
-
-# %% Visualizing standardized dataframe
-
-for i in standard_x.columns:
-    plt.figure(figsize=(10, 6))
-    sns.histplot(standard_x[i], kde=True, bins=30, color='blue')
-    plt.title(f'{i} Distribution')
-    plt.xlabel(i)
-    plt.ylabel('Frequency')
-    plt.show()
-
-for i in standard_x.columns:
-    plt.figure(figsize=(10, 6))
-    sns.stripplot(standard_x[i], alpha=0.7, color='blue')
-    plt.title(f'{i} Distribution')
-    plt.xlabel(i)
-    plt.ylabel('Frequency')
-    plt.show()
-
-
-for col in standard_x.select_dtypes(include=['float', 'int']).columns:
-    print(f"{col}:")
-    print(standard_x[col].describe())
-    print("\n") 
-
 
 # %% Decision Tree parameters
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import GridSearchCV
 
 params = {
-    'max_depth': [3, 5, 10, None],
+    'max_depth': [3, 5, 10, 15, None],
     'min_samples_split': [2, 5, 10],
     'min_samples_leaf': [1, 2, 5],
     'criterion': ['gini', 'entropy']
@@ -168,29 +133,26 @@ grid = GridSearchCV(
     n_jobs=-1
 )
 
-grid.fit(X_train_split, y_train_split)
+grid.fit(X_train, y_train)
 
 print("Best parameters:", grid.best_params_)
 print("Best score:", grid.best_score_)
-#En iyi parametreler: {'criterion': 'entropy', 'max_depth': 5, 'min_samples_leaf': 1, 'min_samples_split': 2}
-#En iyi skor: 0.8506437822762966
+# Best parameters: {'criterion': 'entropy', 'max_depth': 10, 'min_samples_leaf': 5, 'min_samples_split': 2}
+# Best score: 0.942447830403931
 
 # %% Training Decision Tree
-model = DecisionTreeClassifier(criterion='entropy', max_depth=5, random_state=42, min_samples_leaf=1, min_samples_split=2, class_weight='balanced')
-model.fit(X_train_split, y_train_split)
-y_pred = model.predict(X_val_split)
-f1 = f1_score(y_val_split, y_pred, average='weighted')
+model = DecisionTreeClassifier(criterion='entropy', max_depth=10, random_state=42, min_samples_leaf=5, min_samples_split=2, class_weight='balanced')
+model.fit(X_train, y_train)
+y_pred = model.predict(X_val)
+f1 = f1_score(y_val, y_pred, average='weighted')
 print(f"F1 score: {f1:.2f}")
-#F1 score: 0.81
-roc_auc = roc_auc_score(y_val_split, y_pred)
+roc_auc = roc_auc_score(y_val, y_pred)
 print(f"ROC AUC score: {roc_auc:.2f}")
-# ROC AUC score: 0.78
-
-fpr, tpr, thresholds = roc_curve(y_val_split, y_pred)
+fpr, tpr, thresholds = roc_curve(y_val, y_pred)
 
 plt.figure(figsize=(8, 6))
 plt.plot(fpr, tpr, label=f"ROC AUC = {roc_auc:.2f}")
-plt.plot([0, 1], [0, 1], 'k--', label="RAndom Guess (AUC = 0.50)")
+plt.plot([0, 1], [0, 1], 'k--', label="Random Guess (AUC = 0.50)")
 plt.xlabel("False Positive Rate")
 plt.ylabel("True Positive Rate")
 plt.title("ROC Curve")
@@ -202,119 +164,22 @@ plt.show()
 importances = model.feature_importances_
 print(importances)
 plt.figure(figsize=(10,6))
-sns.barplot(x=importances, y=X_train_split.columns)
+sns.barplot(x=importances, y=X_train.columns)
 plt.title("Feature Importance")
 plt.show()
 
-# [0.2339294  0.01779955 0.00325999 0.         0.0090155  0.0160985   0.         0.71989706]
+# [RevolvingUtilizationOfUnsecuredLines,age,DebtRatio,MonthlyIncome,NumberOfOpenCreditLinesAndLoans,NumberRealEstateLoansOrLines,NumberOfDependents, PC1]
+# [0.37774854 0.01392079 0.02087616 0.0118631  0.00957752 0.01584375 0.38289778 0.16727235]
+from sklearn.tree import plot_tree
 
-tree_rules = export_text(
-    model, 
-    feature_names=list(X_train_split.columns)  # Özellik isimlerini belirt
-)
-print(tree_rules)
-"""
-|--- PC1 <= -0.03
-|   |--- RevolvingUtilizationOfUnsecuredLines <= -0.02
-|   |   |--- NumberRealEstateLoansOrLines <= 1.31
-|   |   |   |--- RevolvingUtilizationOfUnsecuredLines <= -0.02
-|   |   |   |   |--- age <= 0.35
-|   |   |   |   |   |--- class: 0
-|   |   |   |   |--- age >  0.35
-|   |   |   |   |   |--- class: 0
-|   |   |   |--- RevolvingUtilizationOfUnsecuredLines >  -0.02
-|   |   |   |   |--- NumberOfOpenCreditLinesAndLoans <= 0.01
-|   |   |   |   |   |--- class: 0
-|   |   |   |   |--- NumberOfOpenCreditLinesAndLoans >  0.01
-|   |   |   |   |   |--- class: 0
-|   |   |--- NumberRealEstateLoansOrLines >  1.31
-|   |   |   |--- DebtRatio <= -0.17
-|   |   |   |   |--- age <= -1.34
-|   |   |   |   |   |--- class: 1
-|   |   |   |   |--- age >  -1.34
-|   |   |   |   |   |--- class: 0
-|   |   |   |--- DebtRatio >  -0.17
-|   |   |   |   |--- NumberRealEstateLoansOrLines <= 3.08
-|   |   |   |   |   |--- class: 0
-|   |   |   |   |--- NumberRealEstateLoansOrLines >  3.08
-|   |   |   |   |   |--- class: 1
-|   |--- RevolvingUtilizationOfUnsecuredLines >  -0.02
-|   |   |--- RevolvingUtilizationOfUnsecuredLines <= -0.02
-|   |   |   |--- NumberOfOpenCreditLinesAndLoans <= 0.98
-|   |   |   |   |--- age <= 0.22
-|   |   |   |   |   |--- class: 0
-|   |   |   |   |--- age >  0.22
-|   |   |   |   |   |--- class: 0
-|   |   |   |--- NumberOfOpenCreditLinesAndLoans >  0.98
-|   |   |   |   |--- age <= -0.26
-|   |   |   |   |   |--- class: 1
-|   |   |   |   |--- age >  -0.26
-|   |   |   |   |   |--- class: 0
-|   |   |--- RevolvingUtilizationOfUnsecuredLines >  -0.02
-|   |   |   |--- age <= 0.49
-|   |   |   |   |--- NumberOfOpenCreditLinesAndLoans <= 0.40
-|   |   |   |   |   |--- class: 1
-|   |   |   |   |--- NumberOfOpenCreditLinesAndLoans >  0.40
-|   |   |   |   |   |--- class: 1
-|   |   |   |--- age >  0.49
-|   |   |   |   |--- age <= 2.25
-|   |   |   |   |   |--- class: 0
-|   |   |   |   |--- age >  2.25
-|   |   |   |   |   |--- class: 0
-|--- PC1 >  -0.03
-|   |--- PC1 <= 0.09
-|   |   |--- RevolvingUtilizationOfUnsecuredLines <= -0.02
-|   |   |   |--- PC1 <= 0.01
-|   |   |   |   |--- RevolvingUtilizationOfUnsecuredLines <= -0.02
-|   |   |   |   |   |--- class: 0
-|   |   |   |   |--- RevolvingUtilizationOfUnsecuredLines >  -0.02
-|   |   |   |   |   |--- class: 1
-|   |   |   |--- PC1 >  0.01
-|   |   |   |   |--- age <= -0.93
-|   |   |   |   |   |--- class: 1
-|   |   |   |   |--- age >  -0.93
-|   |   |   |   |   |--- class: 1
-|   |   |--- RevolvingUtilizationOfUnsecuredLines >  -0.02
-|   |   |   |--- PC1 <= 0.01
-|   |   |   |   |--- RevolvingUtilizationOfUnsecuredLines <= -0.02
-|   |   |   |   |   |--- class: 1
-|   |   |   |   |--- RevolvingUtilizationOfUnsecuredLines >  -0.02
-|   |   |   |   |   |--- class: 1
-|   |   |   |--- PC1 >  0.01
-|   |   |   |   |--- RevolvingUtilizationOfUnsecuredLines <= -0.02
-|   |   |   |   |   |--- class: 1
-|   |   |   |   |--- RevolvingUtilizationOfUnsecuredLines >  -0.02
-|   |   |   |   |   |--- class: 1
-|   |--- PC1 >  0.09
-|   |   |--- PC1 <= 0.17
-|   |   |   |--- RevolvingUtilizationOfUnsecuredLines <= -0.02
-|   |   |   |   |--- NumberOfOpenCreditLinesAndLoans <= -0.19
-|   |   |   |   |   |--- class: 1
-|   |   |   |   |--- NumberOfOpenCreditLinesAndLoans >  -0.19
-|   |   |   |   |   |--- class: 1
-|   |   |   |--- RevolvingUtilizationOfUnsecuredLines >  -0.02
-|   |   |   |   |--- RevolvingUtilizationOfUnsecuredLines <= -0.02
-|   |   |   |   |   |--- class: 1
-|   |   |   |   |--- RevolvingUtilizationOfUnsecuredLines >  -0.02
-|   |   |   |   |   |--- class: 1
-|   |   |--- PC1 >  0.17
-|   |   |   |--- RevolvingUtilizationOfUnsecuredLines <= -0.02
-|   |   |   |   |--- PC1 <= 0.33
-|   |   |   |   |   |--- class: 1
-|   |   |   |   |--- PC1 >  0.33
-|   |   |   |   |   |--- class: 1
-|   |   |   |--- RevolvingUtilizationOfUnsecuredLines >  -0.02
-|   |   |   |   |--- PC1 <= 0.41
-|   |   |   |   |   |--- class: 1
-|   |   |   |   |--- PC1 >  0.41
-|   |   |   |   |   |--- class: 1
-"""
-
+plt.figure(figsize=(20,10))
+plot_tree(model, feature_names=X_train.columns, filled=True, fontsize=10)
+plt.show()
 
 
 #%% Checking covariance similarity for linear discrimination
-cov_mat_0 = X_train_split[y_train_split == 0].cov()  
-cov_mat_1 = X_train_split[y_train_split == 1].cov()  
+cov_mat_0 = X_train[y_train == 0].cov()  
+cov_mat_1 = X_train[y_train == 1].cov()  
 
 print("Covariance Matrix for SeriousDlqin2yrs 0:\n", cov_mat_0, "\n")
 print("Covariance Matrix for SeriousDlqin2yrs 1:\n", cov_mat_1, "\n")
@@ -328,27 +193,92 @@ plt.figure(figsize=(8, 6))
 sns.heatmap(cov_mat_1, annot=True, fmt=".2f", cmap='coolwarm', square=True, cbar_kws={"shrink": .8})
 plt.title('Covariance Matrix HeatMap for SeriousDlqin2yrs 1')
 plt.show()
-# This variance difference leads to QDA
 
 # Methods to formulize this difference
-
 frobenius_norm = np.linalg.norm(cov_mat_0 - cov_mat_1, ord='fro')
 print(f"Frobenius Norm of Covariance Matrix Difference: {frobenius_norm}")
-#Frobenius Norm of Covariance Matrix Difference: 7.748443365369917
+#Frobenius Norm of Covariance Matrix Difference: 2.4780028826650056
 
 det_0 = np.linalg.det(cov_mat_0)
 det_1 = np.linalg.det(cov_mat_1)
 print(f"Determinant of Covariance Matrix for SeriousDlqin2yrs 0: {det_0}")
 print(f"Determinant of Covariance Matrix for SeriousDlqin2yrs 1: {det_1}")
-#Determinant of Covariance Matrix for SeriousDlqin2yrs 0: 0.3784817787643834
-# Determinant of Covariance Matrix for SeriousDlqin2yrs 1: 0.11852843919289058
+#Determinant of Covariance Matrix for SeriousDlqin2yrs 0: 0.1702935509572893
+#Determinant of Covariance Matrix for SeriousDlqin2yrs 1: 0.02177995430110475
 
 eig_vals_0, _ = np.linalg.eig(cov_mat_0)
 eig_vals_1, _ = np.linalg.eig(cov_mat_1)
 
 print(f"Eigenvalues for SeriousDlqin2yrs 0: {eig_vals_0}")
 print(f"Eigenvalues for SeriousDlqin2yrs 1: {eig_vals_1}")
-#Eigenvalues for SeriousDlqin2yrs 0: [1.53642194 1.23659586 1.13759559 1.03374666 0.9277071  0.47606098, 0.53704806 0.71419704]
-#Eigenvalues for SeriousDlqin2yrs 1: [8.07710132 2.0483028  1.22394463 0.20844883 0.18036884 0.72903867, 0.59836647 0.3568883 ]
+#Eigenvalues for SeriousDlqin2yrs 0: [0.1225901  1.6247185  1.29983286 1.37053111 1.41124269 0.54276129 0.81660304 0.76730264]
+#Eigenvalues for SeriousDlqin2yrs 1: [2.09125477 1.48575294 0.98816022 0.20491276 0.27724566 0.63600899 0.42417495 0.46284334]
 
-# %%
+# This variance difference leads to QDA
+# %% QDA
+
+qda = QuadraticDiscriminantAnalysis()
+qda.fit(X_train, y_train)
+y_pred = qda.predict(X_val)
+f1 = f1_score(y_val, y_pred, average='weighted')
+print(f"F1 score: {f1:.2f}")
+roc_auc = roc_auc_score(y_val, y_pred)
+print(f"ROC AUC score: {roc_auc:.2f}")
+
+# %% Outlier Optimization
+
+
+# F1 for decision tree is 0.87, and 0.60 for QDA 
+# ROC AUC for deicision tree is 0.87, and 0.62 for QDA
+# outlier optimization is required
+
+z_scores = np.abs((X_train - X_train.mean()) / X_train.std())
+outliers_z = (z_scores > 3)
+outlier_counts_z = outliers_z.sum(axis=1)
+print("Z-Score Yöntemi ile Aykırı Değer Sayısı:")
+print(outlier_counts_z)
+
+# [RevolvingUtilizationOfUnsecuredLines,age,DebtRatio,MonthlyIncome,NumberOfOpenCreditLinesAndLoans,NumberRealEstateLoansOrLines,NumberOfDependents, PC1]
+# [0.2339294  0.01779955 0.00325999 0.         0.0090155  0.0160985   0.         0.71989706]
+
+Q1 = X_train.quantile(0.25)
+Q3 = X_train.quantile(0.75)
+IQR = Q3 - Q1
+outliers_iqr = (X_train < (Q1 - 1.5 * IQR)) | (X_train > (Q3 + 1.5 * IQR))
+outlier_counts_iqr = outliers_iqr.sum()
+print("\nIQR Yöntemi ile Aykırı Değer Sayısı:")
+print(outlier_counts_iqr)
+
+for col in X_train.columns:
+    if col == "age":
+      continue
+    else:
+      Q1 = X_train[col].quantile(0.25)
+      Q3 = X_train[col].quantile(0.75)
+      IQR = Q3 - Q1
+      upper_limit = Q3 + 1.5 * IQR
+      X_train[col] = np.where(X_train[col] > upper_limit, upper_limit, X_train[col])
+
+
+z_scores = np.abs((X_train - X_train.mean()) / X_train.std())
+outliers_z = (z_scores > 3)
+outlier_counts_z = outliers_z.sum()
+print("Z-Score Yöntemi ile Aykırı Değer Sayısı:")
+print(outlier_counts_z)
+
+Q1 = X_train.quantile(0.25)
+Q3 = X_train.quantile(0.75)
+IQR = Q3 - Q1
+outliers_iqr = (X_train < (Q1 - 1.5 * IQR)) | (X_train > (Q3 + 1.5 * IQR))
+outlier_counts_iqr = outliers_iqr.sum()
+print("\nIQR Yöntemi ile Aykırı Değer Sayısı:")
+print(outlier_counts_iqr)
+
+for i in X_train.columns:
+    plt.figure(figsize=(10, 6))
+    sns.histplot(X_train[i], kde=True, bins=30, color='blue')
+    plt.title(f'{i} Distribution')
+    plt.xlabel(i)
+    plt.ylabel('Frequency')
+    plt.show()
+
