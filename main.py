545376b2010702b85d3997f05f51d8772518639b
Ahsen Altaş <ahsenaltas@Ahsen-MacBook-Air.local>
2025-07-28 22:53:41 +0300

    tried outlier removal instead of SMOTE to preserve dataset, but found it removed important class-1 samples

--- main.py
+++ main.py
@@ -14,29 +14,68 @@
 # **NumberOfTime60-89DaysPastDueNotWorse** <br/>
 # **NumberOfDependents** <br/>
 
-# %% [markdown]
-# Model Results <br/> <br/>
-# Before outlier correction <br/>
-# Decision Tree: F1 = 0.81, ROC AUC = 0.78 <br/>
-# QDA: F1 = 0.60, ROC AUC = 0.62
-
-# %%
+# %% Libraries
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from imblearn.over_sampling import SMOTE
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier,export_text
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-from sklearn.metrics import f1_score, roc_auc_score, roc_curve, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.model_selection import StratifiedShuffleSplit, GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis, LinearDiscriminantAnalysis
+from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score, matthews_corrcoef, balanced_accuracy_score, classification_report
 from scipy.stats import zscore
 
 import warnings
 warnings.filterwarnings("ignore")
 
-# %%
+# %% Model Evaluation Metrics Function
+results_df = pd.DataFrame()
+def modified_classification_report(model_name, y_val, y_prob, y_pred_proba, results_df = None):
+    class_metrics = []
+    for cls in [0, 1]:
+        precision = precision_score(y_val, y_prob, pos_label=cls)
+        recall = recall_score(y_val, y_prob, pos_label=cls)
+        f1 = f1_score(y_val, y_prob, pos_label=cls)
+        roc_auc = roc_auc_score(y_val == cls, y_pred_proba)  
+        class_metrics.append({
+            'Model': model_name,
+            'Class': cls,
+            'Precision': round(precision, 3),
+            'Recall': round(recall, 3),
+            'F1 Score': round(f1, 3),
+            'ROC AUC': round(roc_auc, 3),
+        })
+    
+    mcc = matthews_corrcoef(y_val, y_prob)
+    balanced_acc = balanced_accuracy_score(y_val, y_prob)
+    report = classification_report(y_val, y_prob, digits=3)
+    
+    general_metrics = {
+        'Model': model_name,
+        'Class': 'Overall',
+        'Precision': None,  
+        'Recall': None,     
+        'F1 Score': None,   
+        'ROC AUC': None,    
+        'MCC': round(mcc, 3),
+        'Balanced Acc': round(balanced_acc, 3),
+        'Classification_Report': report
+    }
+    
+    df_class_metrics = pd.DataFrame(class_metrics)
+    df_general_metrics = pd.DataFrame([general_metrics])
+    
+    model_results_df = pd.concat([df_class_metrics, df_general_metrics], ignore_index=True)
+
+    if results_df is None:
+        results_df = model_results_df
+    else:
+        results_df = pd.concat([results_df, model_results_df], ignore_index=True)
+    return results_df
+
+
+# %% Data
 df = pd.read_csv("data/cs-training.csv", usecols=lambda column: column != 'Unnamed: 0')
 print(df.info()) # Missing values on MonthlyIncome and NumberOfDependents
 
@@ -52,7 +91,6 @@ for col in df.select_dtypes(include=['float', 'int']).columns:
     print(f"{col}:")
     print(df[col].describe())
     print("\n")
-
 
 # %% Checking if variables are independent
 filtered_df = df[
@@ -90,6 +128,7 @@ print(high_corr_table)
 
 # since the variance is almost 0 in these variables, PCA is not a good idea
 # I will create one variable for these three, so three variable won't dominate the prediction
+
 df['LatePaymentScore'] = (
     df['NumberOfTime30-59DaysPastDueNotWorse'] +
     df['NumberOfTimes90DaysLate'] +
@@ -101,23 +140,17 @@ print(df['LatePaymentScore'].describe())
 
 # %% Defining variables
 scaler = StandardScaler()
-x=df.iloc[:,1:]
+X=df.iloc[:,1:]
 y=df["SeriousDlqin2yrs"]
+standard_X = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
 
-# in SeriousDlqin2yrs, there are 0 around 140000 and 1 around 20000. To fix this imbalance
-smote = SMOTE(random_state=42)
-X_resampled, y_resampled = smote.fit_resample(x, y)
+sss = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
+for train_index, val_index in sss.split(X, y):
+    X_train, X_val = standard_X.iloc[train_index], standard_X.iloc[val_index]    
+    y_train, y_val = y[train_index], y[val_index]
 
-standard_x = pd.DataFrame(scaler.fit_transform(X_resampled), columns=X_resampled.columns)
-
-X_train, X_val, y_train, y_val = train_test_split(
-    standard_x, y_resampled, test_size=0.2, random_state=42
-)
 
 # %% Decision Tree parameters
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import GridSearchCV
-
 params = {
     'max_depth': [3, 5, 10, 15, None],
     'min_samples_split': [2, 5, 10],
@@ -144,20 +177,8 @@ print("Best score:", grid.best_score_)
 model = DecisionTreeClassifier(criterion='entropy', max_depth=10, random_state=42, min_samples_leaf=5, min_samples_split=2, class_weight='balanced')
 model.fit(X_train, y_train)
 y_pred = model.predict(X_val)
-f1 = f1_score(y_val, y_pred, average='weighted')
-print(f"F1 score: {f1:.2f}")
-roc_auc = roc_auc_score(y_val, y_pred)
-print(f"ROC AUC score: {roc_auc:.2f}")
-fpr, tpr, thresholds = roc_curve(y_val, y_pred)
-
-plt.figure(figsize=(8, 6))
-plt.plot(fpr, tpr, label=f"ROC AUC = {roc_auc:.2f}")
-plt.plot([0, 1], [0, 1], 'k--', label="Random Guess (AUC = 0.50)")
-plt.xlabel("False Positive Rate")
-plt.ylabel("True Positive Rate")
-plt.title("ROC Curve")
-plt.legend(loc="lower right")
-plt.show()
+y_pred_proba = model.predict_proba(X_val)[:, 1]
+results_df= modified_classification_report("decision-tree-before-outlier-clean", y_val, y_pred, y_pred_proba, results_df)
 
 #%% Understanding the logic behind decision tree
 
@@ -216,17 +237,20 @@ print(f"Eigenvalues for SeriousDlqin2yrs 1: {eig_vals_1}")
 
 # This variance difference leads to QDA
 # %% QDA
-
 qda = QuadraticDiscriminantAnalysis()
 qda.fit(X_train, y_train)
 y_pred = qda.predict(X_val)
-f1 = f1_score(y_val, y_pred, average='weighted')
-print(f"F1 score: {f1:.2f}")
-roc_auc = roc_auc_score(y_val, y_pred)
-print(f"ROC AUC score: {roc_auc:.2f}")
+y_pred_proba = qda.predict_proba(X_val)[:, 1]
+results_df = modified_classification_report("qda-before-outlier-clean", y_val, y_pred, y_pred_proba, results_df)
+
+# %% LDA
+lda = LinearDiscriminantAnalysis()
+lda.fit(X_train, y_train)
+y_pred = lda.predict(X_val)
+y_pred_proba = lda.predict_proba(X_val)[:, 1]
+results_df = modified_classification_report("lda-before-outlier-clean", y_val, y_pred, y_pred_proba, results_df)
 
 # %% Outlier Optimization
-
 
 # F1 for decision tree is 0.87, and 0.60 for QDA 
 # ROC AUC for deicision tree is 0.87, and 0.62 for QDA
@@ -266,13 +290,8 @@ outlier_counts_z = outliers_z.sum()
 print("Z-Score Yöntemi ile Aykırı Değer Sayısı:")
 print(outlier_counts_z)
 
-Q1 = X_train.quantile(0.25)
-Q3 = X_train.quantile(0.75)
-IQR = Q3 - Q1
-outliers_iqr = (X_train < (Q1 - 1.5 * IQR)) | (X_train > (Q3 + 1.5 * IQR))
-outlier_counts_iqr = outliers_iqr.sum()
-print("\nIQR Yöntemi ile Aykırı Değer Sayısı:")
-print(outlier_counts_iqr)
+# updating standardization to new distribution
+X_train = pd.DataFrame(scaler.fit_transform(X_train), columns=X_train.columns)
 
 for i in X_train.columns:
     plt.figure(figsize=(10, 6))
@@ -282,3 +301,62 @@ for i in X_train.columns:
     plt.ylabel('Frequency')
     plt.show()
 
+
+#%% Checking covariance similarity for linear discrimination
+y_train = y_train.reset_index(drop=True)
+X_train = X_train.reset_index(drop=True)
+
+cov_mat_0 = X_train[y_train == 0].cov()  
+cov_mat_1 = X_train[y_train == 1].cov()  
+
+print("Covariance Matrix for SeriousDlqin2yrs 0:\n", cov_mat_0, "\n")
+print("Covariance Matrix for SeriousDlqin2yrs 1:\n", cov_mat_1, "\n")
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(cov_mat_0, annot=True, fmt=".2f", cmap='coolwarm', square=True, cbar_kws={"shrink": .8})
+plt.title('Covariance Matrix HeatMap for SeriousDlqin2yrs 0')
+plt.show()
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(cov_mat_1, annot=True, fmt=".2f", cmap='coolwarm', square=True, cbar_kws={"shrink": .8})
+plt.title('Covariance Matrix HeatMap for SeriousDlqin2yrs 1')
+plt.show()
+
+# Methods to formulize this difference
+frobenius_norm = np.linalg.norm(cov_mat_0 - cov_mat_1, ord='fro')
+print(f"Frobenius Norm of Covariance Matrix Difference: {frobenius_norm}")
+# Frobenius Norm of Covariance Matrix Difference: 0.516267796621739
+
+det_0 = np.linalg.det(cov_mat_0)
+det_1 = np.linalg.det(cov_mat_1)
+print(f"Determinant of Covariance Matrix for SeriousDlqin2yrs 0: {det_0}")
+print(f"Determinant of Covariance Matrix for SeriousDlqin2yrs 1: {det_1}")
+# Determinant of Covariance Matrix for SeriousDlqin2yrs 0: 5.017480895089983e-18
+# Determinant of Covariance Matrix for SeriousDlqin2yrs 1: 3.9614581847590716e-18
+
+eig_vals_0, _ = np.linalg.eig(cov_mat_0)
+eig_vals_1, _ = np.linalg.eig(cov_mat_1)
+
+print(f"Eigenvalues for SeriousDlqin2yrs 0: {eig_vals_0}")
+print(f"Eigenvalues for SeriousDlqin2yrs 1: {eig_vals_1}")
+#Eigenvalues for SeriousDlqin2yrs 0: [1.28278868e+00 1.16607543e+00 6.86980591e-01 3.68135240e-01 6.92358613e-02 5.50889981e-04 2.70515538e-06 1.28547583e-07]
+#Eigenvalues for SeriousDlqin2yrs 1: [1.28892404e+00 8.97173832e-01 5.41724292e-01 2.77991005e-01 5.52655804e-02 1.61647581e-03 2.33313534e-06 1.09138560e-07]
+
+#This leads to LDA
+
+# %% Training a LDA
+lda = LinearDiscriminantAnalysis()
+lda.fit(X_train, y_train)
+y_pred = lda.predict(X_val)
+y_pred_proba = lda.predict_proba(X_val)[:, 1]
+results_df = modified_classification_report("qda", y_val, y_pred, y_pred_proba, results_df)
+
+# %% Decision Tree
+model = DecisionTreeClassifier(criterion='entropy', max_depth=10, random_state=42, min_samples_leaf=5, min_samples_split=2, class_weight='balanced')
+model.fit(X_train, y_train)
+y_pred = model.predict(X_val)
+y_pred_proba = model.predict_proba(X_val)[:, 1]
+results_df = modified_classification_report("decision-tree", y_val, y_pred, y_pred_proba, results_df)
+
+# %% Comparing Models
+results_df
