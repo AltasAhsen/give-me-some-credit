d233f8f5a6eac5c198cf3dff7907b575e815ebd9
Ahsen Altaş <ahsenaltas@Ahsen-MacBook-Air.local>
2025-07-30 18:05:28 +0300

    added SVM model

--- main.py
+++ main.py
@@ -19,10 +19,15 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from imblearn.over_sampling import SMOTE
+from collections import Counter
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from umap.umap_ import UMAP
 from sklearn.model_selection import StratifiedShuffleSplit, GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis, LinearDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.svm import SVC
 from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score, matthews_corrcoef, balanced_accuracy_score, classification_report
 from scipy.stats import zscore
 
@@ -105,7 +110,6 @@ print(filtered_df)
 df.hist(bins=30, figsize=(15, 10))
 plt.tight_layout()
 plt.show()
-
 print(df.describe())
 
 # %% Pointing out high correlation features
@@ -128,11 +132,12 @@ print(high_corr_table)
 
 # since the variance is almost 0 in these variables, PCA is not a good idea
 # I will create one variable for these three, so three variable won't dominate the prediction
+# 90 Days Late should be more importnat than 30 Days late
 
 df['LatePaymentScore'] = (
     df['NumberOfTime30-59DaysPastDueNotWorse'] +
-    df['NumberOfTimes90DaysLate'] +
-    df['NumberOfTime60-89DaysPastDueNotWorse']
+    2 * df['NumberOfTime60-89DaysPastDueNotWorse'] +
+    3 * df['NumberOfTimes90DaysLate'] 
 )
 df.drop(columns=['NumberOfTime30-59DaysPastDueNotWorse', 'NumberOfTimes90DaysLate', 'NumberOfTime60-89DaysPastDueNotWorse'], inplace=True)
 
@@ -149,6 +154,51 @@ for train_index, val_index in sss.split(X, y):
     X_train, X_val = standard_X.iloc[train_index], standard_X.iloc[val_index]    
     y_train, y_val = y[train_index], y[val_index]
 
+#%% Checking covariance similarity for linear discrimination
+cov_mat_0 = X_train[y_train == 0].cov()  
+cov_mat_1 = X_train[y_train == 1].cov()  
+
+print("Covariance Matrix for SeriousDlqin2yrs 0:\n", cov_mat_0, "\n")
+print("Covariance Matrix for SeriousDlqin2yrs 1:\n", cov_mat_1, "\n")
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(cov_mat_0, annot=True, fmt=".2f", cmap='coolwarm', square=True, cbar_kws={"shrink": .8})
+plt.title('Covariance Matrix HeatMap for SeriousDlqin2yrs 0')
+plt.show()
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(cov_mat_1, annot=True, fmt=".2f", cmap='coolwarm', square=True, cbar_kws={"shrink": .8})
+plt.title('Covariance Matrix HeatMap for SeriousDlqin2yrs 1')
+plt.show()
+
+# Methods to formulize this difference
+frobenius_norm = np.linalg.norm(cov_mat_0 - cov_mat_1, ord='fro')
+print(f"Frobenius Norm of Covariance Matrix Difference: {frobenius_norm}")
+# Frobenius Norm of Covariance Matrix Difference: 7.120624199876019
+
+det_0 = np.linalg.det(cov_mat_0)
+det_1 = np.linalg.det(cov_mat_1)
+print(f"Determinant of Covariance Matrix for SeriousDlqin2yrs 0: {det_0}")
+print(f"Determinant of Covariance Matrix for SeriousDlqin2yrs 1: {det_1}")
+# Determinant of Covariance Matrix for SeriousDlqin2yrs 0: 0.3729770508242348
+# Determinant of Covariance Matrix for SeriousDlqin2yrs 1: 0.16995540178160862
+
+eig_vals_0, _ = np.linalg.eig(cov_mat_0)
+eig_vals_1, _ = np.linalg.eig(cov_mat_1)
+
+print(f"Eigenvalues for SeriousDlqin2yrs 0: {eig_vals_0}")
+print(f"Eigenvalues for SeriousDlqin2yrs 1: {eig_vals_1}")
+# Eigenvalues for SeriousDlqin2yrs 0: [1.51156773 0.45073134 0.53409605 0.71433014 1.23327751 1.13620735 0.95944205 1.06728707]
+# Eigenvalues for SeriousDlqin2yrs 1: [7.40258486 2.16513628 1.22790289 0.1951373  0.35782767 0.27484533 0.75276357 0.59777968]
+
+# This variance difference leads to QDA
+
+# %% QDA
+qda = QuadraticDiscriminantAnalysis()
+qda.fit(X_train, y_train)
+y_pred = qda.predict(X_val)
+y_pred_proba = qda.predict_proba(X_val)[:, 1]
+results_df = modified_classification_report("qda-before-SMOTE", y_val, y_pred, y_pred_proba, results_df)
 
 # %% Decision Tree parameters
 params = {
@@ -170,17 +220,15 @@ grid.fit(X_train, y_train)
 
 print("Best parameters:", grid.best_params_)
 print("Best score:", grid.best_score_)
-# Best parameters: {'criterion': 'entropy', 'max_depth': 10, 'min_samples_leaf': 5, 'min_samples_split': 2}
-# Best score: 0.942447830403931
+# Best parameters: {'criterion': 'entropy', 'max_depth': 5, 'min_samples_leaf': 1, 'min_samples_split': 2}
+# Best score: 0.8494308978342376
 
 # %% Training Decision Tree
-model = DecisionTreeClassifier(criterion='entropy', max_depth=10, random_state=42, min_samples_leaf=5, min_samples_split=2, class_weight='balanced')
+model = DecisionTreeClassifier(criterion='entropy', max_depth=5, random_state=42, min_samples_leaf=1, min_samples_split=2, class_weight='balanced')
 model.fit(X_train, y_train)
 y_pred = model.predict(X_val)
 y_pred_proba = model.predict_proba(X_val)[:, 1]
-results_df= modified_classification_report("decision-tree-before-outlier-clean", y_val, y_pred, y_pred_proba, results_df)
-
-#%% Understanding the logic behind decision tree
+results_df= modified_classification_report("decision-tree-before-SMOTE", y_val, y_pred, y_pred_proba, results_df)
 
 importances = model.feature_importances_
 print(importances)
@@ -189,18 +237,19 @@ sns.barplot(x=importances, y=X_train.columns)
 plt.title("Feature Importance")
 plt.show()
 
-# [RevolvingUtilizationOfUnsecuredLines,age,DebtRatio,MonthlyIncome,NumberOfOpenCreditLinesAndLoans,NumberRealEstateLoansOrLines,NumberOfDependents, PC1]
-# [0.37774854 0.01392079 0.02087616 0.0118631  0.00957752 0.01584375 0.38289778 0.16727235]
-from sklearn.tree import plot_tree
+# %% SMOTE uygulama
+smote = SMOTE(random_state=42)  # Rastgelelik için bir seed belirleyebilirsiniz
+X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
 
-plt.figure(figsize=(20,10))
-plot_tree(model, feature_names=X_train.columns, filled=True, fontsize=10)
-plt.show()
-
+print("Original class distribution:", Counter(y_train))
+print("Resampled class distribution:", Counter(y_train_resampled))
 
 #%% Checking covariance similarity for linear discrimination
-cov_mat_0 = X_train[y_train == 0].cov()  
-cov_mat_1 = X_train[y_train == 1].cov()  
+y_train_resampled = y_train_resampled.reset_index(drop=True)
+X_train_resampled = X_train_resampled.reset_index(drop=True)
+
+cov_mat_0 = X_train_resampled[y_train_resampled == 0].cov()  
+cov_mat_1 = X_train_resampled[y_train_resampled == 1].cov()  
 
 print("Covariance Matrix for SeriousDlqin2yrs 0:\n", cov_mat_0, "\n")
 print("Covariance Matrix for SeriousDlqin2yrs 1:\n", cov_mat_1, "\n")
@@ -218,145 +267,129 @@ plt.show()
 # Methods to formulize this difference
 frobenius_norm = np.linalg.norm(cov_mat_0 - cov_mat_1, ord='fro')
 print(f"Frobenius Norm of Covariance Matrix Difference: {frobenius_norm}")
-#Frobenius Norm of Covariance Matrix Difference: 2.4780028826650056
+# Frobenius Norm of Covariance Matrix Difference: 7.158037685749023
 
 det_0 = np.linalg.det(cov_mat_0)
 det_1 = np.linalg.det(cov_mat_1)
 print(f"Determinant of Covariance Matrix for SeriousDlqin2yrs 0: {det_0}")
 print(f"Determinant of Covariance Matrix for SeriousDlqin2yrs 1: {det_1}")
-#Determinant of Covariance Matrix for SeriousDlqin2yrs 0: 0.1702935509572893
-#Determinant of Covariance Matrix for SeriousDlqin2yrs 1: 0.02177995430110475
+# Determinant of Covariance Matrix for SeriousDlqin2yrs 0: 0.3729770508242348
+# Determinant of Covariance Matrix for SeriousDlqin2yrs 1: 0.07801523023152504
 
 eig_vals_0, _ = np.linalg.eig(cov_mat_0)
 eig_vals_1, _ = np.linalg.eig(cov_mat_1)
 
 print(f"Eigenvalues for SeriousDlqin2yrs 0: {eig_vals_0}")
 print(f"Eigenvalues for SeriousDlqin2yrs 1: {eig_vals_1}")
-#Eigenvalues for SeriousDlqin2yrs 0: [0.1225901  1.6247185  1.29983286 1.37053111 1.41124269 0.54276129 0.81660304 0.76730264]
-#Eigenvalues for SeriousDlqin2yrs 1: [2.09125477 1.48575294 0.98816022 0.20491276 0.27724566 0.63600899 0.42417495 0.46284334]
+# Eigenvalues for SeriousDlqin2yrs 0: [1.51156773 0.45073134 0.53409605 0.71433014 1.23327751 1.13620735 0.95944205 1.06728707]
+# Eigenvalues for SeriousDlqin2yrs 1: [7.42781272 2.10688322 1.21392259 0.29231128 0.24458355 0.14007467 0.72263076 0.56746374]
 
-# This variance difference leads to QDA
-# %% QDA
+#This leads to QDA
+
+# %% Training a QDA
 qda = QuadraticDiscriminantAnalysis()
-qda.fit(X_train, y_train)
+qda.fit(X_train_resampled, y_train_resampled)
 y_pred = qda.predict(X_val)
 y_pred_proba = qda.predict_proba(X_val)[:, 1]
-results_df = modified_classification_report("qda-before-outlier-clean", y_val, y_pred, y_pred_proba, results_df)
-
-# %% LDA
-lda = LinearDiscriminantAnalysis()
-lda.fit(X_train, y_train)
-y_pred = lda.predict(X_val)
-y_pred_proba = lda.predict_proba(X_val)[:, 1]
-results_df = modified_classification_report("lda-before-outlier-clean", y_val, y_pred, y_pred_proba, results_df)
-
-# %% Outlier Optimization
-
-# F1 for decision tree is 0.87, and 0.60 for QDA 
-# ROC AUC for deicision tree is 0.87, and 0.62 for QDA
-# outlier optimization is required
-
-z_scores = np.abs((X_train - X_train.mean()) / X_train.std())
-outliers_z = (z_scores > 3)
-outlier_counts_z = outliers_z.sum(axis=1)
-print("Z-Score Yöntemi ile Aykırı Değer Sayısı:")
-print(outlier_counts_z)
-
-# [RevolvingUtilizationOfUnsecuredLines,age,DebtRatio,MonthlyIncome,NumberOfOpenCreditLinesAndLoans,NumberRealEstateLoansOrLines,NumberOfDependents, PC1]
-# [0.2339294  0.01779955 0.00325999 0.         0.0090155  0.0160985   0.         0.71989706]
-
-Q1 = X_train.quantile(0.25)
-Q3 = X_train.quantile(0.75)
-IQR = Q3 - Q1
-outliers_iqr = (X_train < (Q1 - 1.5 * IQR)) | (X_train > (Q3 + 1.5 * IQR))
-outlier_counts_iqr = outliers_iqr.sum()
-print("\nIQR Yöntemi ile Aykırı Değer Sayısı:")
-print(outlier_counts_iqr)
-
-for col in X_train.columns:
-    if col == "age":
-      continue
-    else:
-      Q1 = X_train[col].quantile(0.25)
-      Q3 = X_train[col].quantile(0.75)
-      IQR = Q3 - Q1
-      upper_limit = Q3 + 1.5 * IQR
-      X_train[col] = np.where(X_train[col] > upper_limit, upper_limit, X_train[col])
-
-
-z_scores = np.abs((X_train - X_train.mean()) / X_train.std())
-outliers_z = (z_scores > 3)
-outlier_counts_z = outliers_z.sum()
-print("Z-Score Yöntemi ile Aykırı Değer Sayısı:")
-print(outlier_counts_z)
-
-# updating standardization to new distribution
-X_train = pd.DataFrame(scaler.fit_transform(X_train), columns=X_train.columns)
-
-for i in X_train.columns:
-    plt.figure(figsize=(10, 6))
-    sns.histplot(X_train[i], kde=True, bins=30, color='blue')
-    plt.title(f'{i} Distribution')
-    plt.xlabel(i)
-    plt.ylabel('Frequency')
-    plt.show()
-
-
-#%% Checking covariance similarity for linear discrimination
-y_train = y_train.reset_index(drop=True)
-X_train = X_train.reset_index(drop=True)
-
-cov_mat_0 = X_train[y_train == 0].cov()  
-cov_mat_1 = X_train[y_train == 1].cov()  
-
-print("Covariance Matrix for SeriousDlqin2yrs 0:\n", cov_mat_0, "\n")
-print("Covariance Matrix for SeriousDlqin2yrs 1:\n", cov_mat_1, "\n")
-
-plt.figure(figsize=(8, 6))
-sns.heatmap(cov_mat_0, annot=True, fmt=".2f", cmap='coolwarm', square=True, cbar_kws={"shrink": .8})
-plt.title('Covariance Matrix HeatMap for SeriousDlqin2yrs 0')
-plt.show()
-
-plt.figure(figsize=(8, 6))
-sns.heatmap(cov_mat_1, annot=True, fmt=".2f", cmap='coolwarm', square=True, cbar_kws={"shrink": .8})
-plt.title('Covariance Matrix HeatMap for SeriousDlqin2yrs 1')
-plt.show()
-
-# Methods to formulize this difference
-frobenius_norm = np.linalg.norm(cov_mat_0 - cov_mat_1, ord='fro')
-print(f"Frobenius Norm of Covariance Matrix Difference: {frobenius_norm}")
-# Frobenius Norm of Covariance Matrix Difference: 0.516267796621739
-
-det_0 = np.linalg.det(cov_mat_0)
-det_1 = np.linalg.det(cov_mat_1)
-print(f"Determinant of Covariance Matrix for SeriousDlqin2yrs 0: {det_0}")
-print(f"Determinant of Covariance Matrix for SeriousDlqin2yrs 1: {det_1}")
-# Determinant of Covariance Matrix for SeriousDlqin2yrs 0: 5.017480895089983e-18
-# Determinant of Covariance Matrix for SeriousDlqin2yrs 1: 3.9614581847590716e-18
-
-eig_vals_0, _ = np.linalg.eig(cov_mat_0)
-eig_vals_1, _ = np.linalg.eig(cov_mat_1)
-
-print(f"Eigenvalues for SeriousDlqin2yrs 0: {eig_vals_0}")
-print(f"Eigenvalues for SeriousDlqin2yrs 1: {eig_vals_1}")
-#Eigenvalues for SeriousDlqin2yrs 0: [1.28278868e+00 1.16607543e+00 6.86980591e-01 3.68135240e-01 6.92358613e-02 5.50889981e-04 2.70515538e-06 1.28547583e-07]
-#Eigenvalues for SeriousDlqin2yrs 1: [1.28892404e+00 8.97173832e-01 5.41724292e-01 2.77991005e-01 5.52655804e-02 1.61647581e-03 2.33313534e-06 1.09138560e-07]
-
-#This leads to LDA
-
-# %% Training a LDA
-lda = LinearDiscriminantAnalysis()
-lda.fit(X_train, y_train)
-y_pred = lda.predict(X_val)
-y_pred_proba = lda.predict_proba(X_val)[:, 1]
 results_df = modified_classification_report("qda", y_val, y_pred, y_pred_proba, results_df)
 
-# %% Decision Tree
-model = DecisionTreeClassifier(criterion='entropy', max_depth=10, random_state=42, min_samples_leaf=5, min_samples_split=2, class_weight='balanced')
-model.fit(X_train, y_train)
-y_pred = model.predict(X_val)
-y_pred_proba = model.predict_proba(X_val)[:, 1]
-results_df = modified_classification_report("decision-tree", y_val, y_pred, y_pred_proba, results_df)
+
+# %% Decision Tree parameters
+params = {
+    'max_depth': [3, 5, 10, 15, None],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 5],
+    'criterion': ['gini', 'entropy']
+}
+
+grid = GridSearchCV(
+    estimator=DecisionTreeClassifier(random_state=42),
+    param_grid=params,
+    cv=5,
+    scoring='roc_auc',
+    n_jobs=-1
+)
+
+grid.fit(X_train_resampled, y_train_resampled)
+
+print("Best parameters:", grid.best_params_)
+print("Best score:", grid.best_score_)
+# Best parameters: {'criterion': 'gini', 'max_depth': 15, 'min_samples_leaf': 5, 'min_samples_split': 2}
+# Best score: 0.9539837789333863
+
+# %% Training Decision Tree
+tree = DecisionTreeClassifier(criterion='gini', max_depth=15, random_state=42, min_samples_leaf=5, min_samples_split=2, class_weight='balanced')
+tree.fit(X_train_resampled, y_train_resampled)
+y_pred = tree.predict(X_val)
+y_pred_proba = tree.predict_proba(X_val)[:, 1]
+results_df= modified_classification_report("decision-tree", y_val, y_pred, y_pred_proba, results_df)
+
+#%% Understanding the logic behind decision tree
+importances = tree.feature_importances_
+feature_importance_df = pd.DataFrame({
+    'feature': X_train_resampled.columns, 
+    'importance': importances
+})
+
+plt.figure(figsize=(10,6))
+sns.barplot(x=importances, y=X_train.columns)
+plt.title("Feature Importance")
+plt.show()
+
+# %% SVM kernel selection
+# Method-1 : Interpreting Features of Decision Tree
+
+# 1. Dominance Ratio
+class_counts = np.bincount(y_train_resampled)
+dominance_ratio = class_counts.max() / class_counts.sum()
+
+# 2. Gini Impurity
+classes, counts = np.unique(y_train_resampled, return_counts=True)
+probs = counts / counts.sum()
+gini = 1 - np.sum(probs**2)
+
+# 3. Tree Depth
+tree_depth = tree.get_depth()
+
+# 4. Complexity Score
+n_leaves = model.get_n_leaves()
+complexity_score = tree_depth * np.log(n_leaves)
+
+print("Dominance_ratio: ", dominance_ratio, "\nGini: ", gini, "\nDepth: ", tree_depth, "\nComplexity: ", complexity_score)
+# Dominance_ratio:  0.5, Gini:  0.5, Depth:  15 , Complexity:  51.986038541995896 : It's a deep tree which means-non-linear relationships
+
+# Method-2: Interpret Dimension Reduction
+# PCA
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X_train_resampled)
+# UMAP
+umap_model = UMAP(n_components=2, random_state=42)
+X_umap = umap_model.fit_transform(X_train_resampled)
+
+# Visualization
+fig, axs = plt.subplots(1, 2, figsize=(12, 5))
+target_names = [f"Class {i}" for i in np.unique(y)]
+
+# #  PCA
+axs[0].set_title("PCA")
+for label in np.unique(y):
+    axs[0].scatter(X_pca[y_train_resampled == label, 0], X_pca[y_train_resampled == label, 1], label=target_names[label], alpha=0.6)
+axs[0].legend()
+
+# # UMAP
+axs[1].set_title("UMAP")
+for label in np.unique(y):
+    axs[1].scatter(X_umap[y_train_resampled == label, 0], X_umap[y_train_resampled == label, 1], label=target_names[label], alpha=0.6)
+axs[1].legend()
+
+plt.show()
+
+# %% SVM
+svm_model = SVC(kernel="rbf", random_state=42, probability=True)
+svm_model.fit(X_train_resampled, y_train_resampled)
+y_pred = svm_model.predict(X_val)
+y_pred_proba = svm_model.predict_proba(X_val)[:, 1]
+results_df= modified_classification_report("SVM", y_val, y_pred, y_pred_proba, results_df)
 
 # %% Comparing Models
 results_df
